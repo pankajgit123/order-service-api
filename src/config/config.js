ca0496d977e703b764a9c08ef982cac76571adf7
Pankaj Khadse <pankajmac@Pankajs-MacBook-Pro.local>
2025-02-23 15:22:26 -0500

    verify env variables whole loading app

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+// update if any change in .env
+const expectedEnvVariables = [
+  "POSTGRESDB_USER",
+  "POSTGRESDB_ROOT_PASSWORD",
+  "POSTGRESDB_DATABASE",
+  "POSTGRESDB_LOCAL_PORT",
+  "POSTGRESDB_DOCKER_PORT",
+  "NODE_LOCAL_PORT",
+  "NODE_DOCKER_PORT",
+];
+
+if (process.env) {
+  expectedEnvVariables.forEach((element) => {
+    if (!process.env[element]) {
+      console.log(
+        `Config value is not available for '${element}'`,
+        process.env[element]
+      );
+      process.exit(1);
+    }
+  });
+}
+
 export const config = Object.freeze({
   env: process.env.NODE_ENV,
   port: process.env.NODE_LOCAL_PORT,
